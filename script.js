46b4517a7a478103bef01c935c9a58a42226fc46
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2023-10-01 12:33:38 -0500

    create a function to run charactor with enter key

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function idleAnimation(){
     girl.src = "resources/idle (" + idleImageNumber + ").png";
 }
 
+//Idle animation start funtion
 function idleAnimationStart(){
 idleAnimationNumber = setInterval(idleAnimation, 200);
 }
@@ -19,7 +20,7 @@ idleAnimationNumber = setInterval(idleAnimation, 200);
 //run animaition function
 
 runImageNumber = 0;
-
+runAnimationNumber = 0;
 function runAnimation()
 {
 runImageNumber = runImageNumber + 1;
@@ -29,3 +30,22 @@ if( idleImageNumber == 11){
 girl.src = "resources/Run (" + runImageNumber + ").png";
 }
 
+//run animation start funtion
+function runAnimationStart(){
+    runAnimationNumber = setInterval(runAnimation, 100);
+    clearInterval(idleAnimationNumber);
+}
+//run charactor with enterkey press
+
+function keyCheck(event){
+//alert(event.which);
+//enter =13;
+
+const key=event.which;
+
+if(key == 13){
+    if(runAnimationNumber == 0){
+       runAnimationStart();
+    }
+  }
+}
