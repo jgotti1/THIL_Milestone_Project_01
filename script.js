870d10a0b7960fc30d886e3978b02b4292753e69
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2023-10-01 11:13:39 -0500

    create Idle animation function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//this my js
+//Idle animation function
+const girl = document.getElementById("girl");
+idleImageNumber = 0;
+idleAnimationNumber = 0;
+
+function idleAnimation(){
+    idleImageNumber = idleImageNumber + 1;
+    
+    if( idleImageNumber == 11){
+        idleImageNumber = 1;
+    }
+    girl.src = "resources/idle (" + idleImageNumber + ").png";
+ console.log(girl.src);
+}
+
+function idleAnimationStart(){
+idleAnimationNumber = setInterval(idleAnimation, 200);
+}
